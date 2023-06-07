62aa0a8c400396e02bc6f257357548a9a7913782
timofeykt <kucmarevtima@gmail.com>
2023-06-07 17:44:21 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "main.h"
+int main() {
+	/*ofstream fout;
+
+
+	fout.open("text.txt");
+
+if (fout.is_open()) {
+	fout << "testing....." << "testing....." << "testing.....";
+
+	fout.close();
+
+}*/
+
+
+
+	return 0;
+}
